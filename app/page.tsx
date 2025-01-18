5014154aa4b26aa9d10f385a62f11910a85e4f36
Enock Chikobvu <92862163+enock-chi@users.noreply.github.com>
2025-01-18 11:10:50 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="pt-[12vh] grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="w-full pt-[24vh] flex">
+      <div className="w-full h-[50vh] pl-[6vw] pt-[24vh] flex">
         <Image src='/ivytext.PNG' alt='ivyprojects' width={300} height={100} className="z-1"/>
-        <Image src='/ivyflower.PNG' alt="" width={100} height={100} className="absolute top-[12vh] right-[6vw]  z-10" />
-        <Image src='/ivytape.PNG' alt="" width={100} height={100} className="absolute top-[20vh] left-[-2vw] z-10"/>
+        <Image src='/ivyflower.PNG' alt="" width={100} height={100} className="absolute top-[12vh] right-[3vw]  z-10" />
+        <Image src='/ivytape.PNG' alt="" width={100} height={100} className="absolute top-[20vh] left-[4vw] z-10"/>
+      </div>
+      <div className="">
+
       </div>
     </div>
   );
