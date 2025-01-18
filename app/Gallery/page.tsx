5dc474c5597f00584ba6f5d8b20de98b27ec3dba
Enock Chikobvu <92862163+enock-chi@users.noreply.github.com>
2025-01-18 10:53:32 +0200

    layout for mobile

---
+++ app/Gallery/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type PageProps = {};
+
+const Page: React.FC<PageProps> = () => {
+    return (
+        <div>
+            Page
+        </div>
+    );
+};
+
+export default Page;
